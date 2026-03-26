834850be81d4c88934921c4e9bf3c1953b29bd68
liza962 <lizabajrami962@gmail.com>
2026-03-26 12:00:31 +0100

    Inntegrated React-Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
 import { Link, Route, Routes } from "react-router-dom";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
@@ -6,18 +7,34 @@ import Home from "./pages/Home.jsx";
 export default function App() {
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        {" · "}
-        <Link to="/about">About us</Link>
-        {" · "}
-        <Link to="/contact">Contact us</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Navbar bg="light" expand="lg" className="mb-3">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Glowelle
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="main-nav" />
+          <Navbar.Collapse id="main-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About us
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact us
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Container>
     </>
   );
 }
